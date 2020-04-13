e12852c719ddc9f224dcb2043f7fd92dd4c39758
Tharikh <tharikhlegend@gmail.com>
2020-04-13 23:38:14 +0530

    completed pointer

--- pointers_advanced/pointer.c
+++ pointers_advanced/pointer.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+#include<assert.h>
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
-
 char *kth_word_in_mth_sentence_of_nth_paragraph(char ****document, int k, int m, int n)
 {
-    return *(*(*(document + n) + m) + k);
+    return *(*(*(document + (n-1)) + (m-1)) + (k-1));
 }
 
 char **kth_sentence_in_mth_paragraph(char ****document, int k, int m)
 {
-    return *(*(document + m) + k);
+    return *(*(document + (m-1)) + (k-1));
 }
 
 char ***kth_paragraph(char ****document, int k)
 {
-    return *(document + k);
+    return *(document + (k-1));
 }
 
 char ****get_document(char *text)
@@ -33,117 +32,117 @@ char ****get_document(char *text)
         switch (text[i])
         {
         case ' ':
+            *(*(*(*(document + (para - 1)) + (sent - 1)) + (word - 1)) + (letter - 1)) ='\0';
             word++;
-            *(*(document +(para-1)) + (sent-1)) = (char **)realloc(*(*(document + (para-1)) + (sent-1)), word * sizeof(char *));
-            letter=1;
+            *(*(document + (para - 1)) + (sent - 1)) = (char **)realloc(*(*(document + (para - 1)) + (sent - 1)), word * sizeof(char *));
+            letter = 1;
+            *(*(*(document+(para-1))+(sent-1))+(word-1)) = (char *)malloc(sizeof(char));
             break;
         case '.':
             sent++;
-            *(document + (para-1)) = (char ***)realloc(*(document + (para-1)), sent * sizeof(char **));
-            word=1;
-            letter=1;
+            // *(*(document + (para - 1)) + (sent - 2)) = (char **)realloc(*(*(document + (para - 1)) + (sent - 1)), (word-1) * sizeof(char *));
+            *(document + (para - 1)) = (char ***)realloc(*(document + (para - 1)), sent * sizeof(char **));
+            word = 1;
+            letter = 1;
+            *(*(document+(para-1))+(sent-1)) = (char **)malloc(sizeof(char *));
+            *(*(*(document+(para-1))+(sent-1))+(word-1)) = (char *)malloc(sizeof(char));
             break;
         case '\n':
             para++;
-            document = (char ****)realloc(document, para*sizeof(char ***));
-            sent=1;
-            word=1;
-            letter=1;
+            document = (char ****)realloc(document, para * sizeof(char ***));
+            // *(document + (para - 2)) = (char ***)realloc(*(document + (para - 1)), (sent-1) * sizeof(char **));
+            sent = 1;
+            word = 1;
+            letter = 1;
+            *(document+(para-1)) = (char ***)malloc(sizeof(char **));
+            *(*(document+(para-1))+(sent-1)) = (char **)malloc(sizeof(char *));
+            *(*(*(document+(para-1))+(sent-1))+(word-1)) = (char *)malloc(sizeof(char));
             break;
         default:
-            *(*(*(document+(para-1))+(sent-1))+(word-1))=(char *)realloc(*(*(*(document+(para-1))+(sent-1))+(word-1)),(letter)*sizeof(char));
-            *(*(*(*(document+(para-1))+(sent-1))+(word-1))+(letter-1))=text[i];
             letter++;
+            *(*(*(document + (para - 1)) + (sent - 1)) + (word - 1)) = (char *)realloc(*(*(*(document + (para - 1)) + (sent - 1)) + (word - 1)), (letter) * sizeof(char));
+            *(*(*(*(document + (para - 1)) + (sent - 1)) + (word - 1)) + (letter - 2)) = text[i];
         }
     }
     return document;
 }
 
-char *get_input_text()
-{
+
+
+char* get_input_text() {	
     int paragraph_count;
     scanf("%d", &paragraph_count);
 
     char p[MAX_PARAGRAPHS][MAX_CHARACTERS], doc[MAX_CHARACTERS];
     memset(doc, 0, sizeof(doc));
     getchar();
-    for (int i = 0; i < paragraph_count; i++)
-    {
+    for (int i = 0; i < paragraph_count; i++) {
         scanf("%[^\n]%*c", p[i]);
         strcat(doc, p[i]);
         if (i != paragraph_count - 1)
             strcat(doc, "\n");
     }
 
-    char *returnDoc = (char *)malloc((strlen(doc) + 1) * (sizeof(char)));
+    char* returnDoc = (char*)malloc((strlen (doc)+1) * (sizeof(char)));
     strcpy(returnDoc, doc);
     return returnDoc;
 }
 
-void print_word(char *word)
-{
+void print_word(char* word) {
     printf("%s", word);
 }
 
-void print_sentence(char **sentence)
-{
+void print_sentence(char** sentence) {
     int word_count;
     scanf("%d", &word_count);
-    for (int i = 0; i < word_count; i++)
-    {
+    for(int i = 0; i < word_count; i++){
         printf("%s", sentence[i]);
-        if (i != word_count - 1)
+        if( i != word_count - 1)
             printf(" ");
     }
-}
+} 
 
-void print_paragraph(char ***paragraph)
-{
+void print_paragraph(char*** paragraph) {
     int sentence_count;
     scanf("%d", &sentence_count);
-    for (int i = 0; i < sentence_count; i++)
-    {
+    for (int i = 0; i < sentence_count; i++) {
         print_sentence(*(paragraph + i));
         printf(".");
     }
 }
 
-int main()
+int main() 
 {
-    char *text = get_input_text();
-    char ****document = get_document(text);
+    char* text = get_input_text();
+    char**** document = get_document(text);
 
     int q;
     scanf("%d", &q);
 
-    while (q--)
-    {
+    while (q--) {
         int type;
         scanf("%d", &type);
 
-        if (type == 3)
-        {
+        if (type == 3){
             int k, m, n;
             scanf("%d %d %d", &k, &m, &n);
-            char *word = kth_word_in_mth_sentence_of_nth_paragraph(document, k, m, n);
+            char* word = kth_word_in_mth_sentence_of_nth_paragraph(document, k, m, n);
             print_word(word);
         }
 
-        else if (type == 2)
-        {
+        else if (type == 2){
             int k, m;
             scanf("%d %d", &k, &m);
-            char **sentence = kth_sentence_in_mth_paragraph(document, k, m);
+            char** sentence = kth_sentence_in_mth_paragraph(document, k, m);
             print_sentence(sentence);
         }
 
-        else
-        {
+        else{
             int k;
             scanf("%d", &k);
-            char ***paragraph = kth_paragraph(document, k);
+            char*** paragraph = kth_paragraph(document, k);
             print_paragraph(paragraph);
         }
         printf("\n");
-    }
+    }     
 }
